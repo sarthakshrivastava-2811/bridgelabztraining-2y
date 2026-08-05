7c2327dafe688538056ee590b2021e9d1fe9975d
Sarthak Shrivastava <somkhare53@gmail.com>
2026-08-05 23:16:56 +0530

    hello

---
+++ JAVADSA/src/Arrays/leetcode/leetcode189.java
@@ -0,0 +1,41 @@
+package Arrays.leetcode;
+import java.util.Scanner;
+
+public class leetcode189 {
+
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public static void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+
+        System.out.print("Enter the value of k: ");
+        int k = sc.nextInt();
+        rotate(nums, k);
+        System.out.println("Rotated Array:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
